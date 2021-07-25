43c14c1e8ad0dd2b94c33d2fdd42e6f8d7e540dc
Fer Wicker <fer.wicker@gmail.com>
2021-07-25 12:41:35 +1000

    clean up comparisons for hands

--- index.js
+++ index.js
@@ -10,19 +10,25 @@ let allGames = [];
 let playerOneCount = 0;
 let playerTwoCount = 0;
 
+let playerHands = [];
+
 let playerCards;
 
-function separatePlayers(hand){
-    playerCards = [];
-    playerCards.push(hand.slice(0,5), hand.slice(5));
-    playerCards.forEach(e => { //sort cards by value
+function sortValue (x){
+    x.forEach(e => { //sort by numeric value
         e.sort(function(a, b) {
             return a.value - b.value;
           });
     })
 }
 
-function numberValue(value){
+function separatePlayers(hand){
+    playerCards = [];
+    playerCards.push(hand.slice(0,5), hand.slice(5));
+    sortValue(playerCards);
+}
+
+function getNumValue(value){
     let number = parseInt(value);
     if (isNaN(number) === true){
         switch (value){
@@ -54,7 +60,7 @@ function getCardsObject(cards){
         cardsArr.push(allCards[i].split(''));
         cardsObj = cardsArr.map(function(x){
             return {
-                value: numberValue(x[0]),
+                value: getNumValue(x[0]),
                 suit: x[1]
             }
         });
@@ -62,49 +68,85 @@ function getCardsObject(cards){
     return cardsObj;
 }
 
+function checkConsecutive(x){
+    for (i=0; i<3; i++){
+        var j = i + 1;
+        var diff = x[i].value - x[j].value; //breaks here after certain amount of times running, cant find second value
+        if(diff !== -1){
+            consecutive = false;
+            break
+        } else {
+            consecutive = true;
+        }
+    }
+}
+
+function checkSameSuit(x){
+    for (i=0; i<3; i++){
+        var j = i + 1;
+        if (x[i].suit !== x[j].suit){
+            sameSuit = false;
+            break
+        } else {
+            sameSuit = true;
+        }
+    }
+}
+
+function RoyalFlush(x, index){
+    if (consecutive === true && sameSuit === true){
+        if (x[4].value === 14) {
+            playerHands[index].handValue = 10;
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+function StraightFlush(x, index){
+    if (consecutive === true && sameSuit === true){
+        if (x[4].value < 14) {
+            playerHands[index].handValue = 9;
+            playerHands[index].highValue = x[4].value;
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+function Flush (x, index){
+    if (consecutive === false && sameSuit === true) {
+        playerHands[index].handValue = 6;
+        playerHands[index].highValue = x[4].value;
+        return true;
+    } else {
+        return false
+    }
+}
+
+function Straight (x, index){
+    if (consecutive === true && sameSuit === false) {
+        playerHands[index].handValue = 5;
+        playerHands[index].highValue = x[4].value;
+        return true;
+    } else {
+        return false
+    }
+}
+
 function checkHands(x){
-    x.forEach(element => {
-        var i = 0;
-        console.log(element);
-        console.log('\n\n')
+    x.forEach((element, index) => {
+        playerHands.push({});
         let consecutive;
         let sameSuit;
-        for (i=0; i<3; i++){
-            var j = i + 1;
-            var diff = element[i].value - element[j].value;
-            if(diff !== -1){
-                consecutive = false;
-                break
-            } else {
-                consecutive = true;
-            }
-        }
-        for (i=0; i<3; i++){
-            var j = i + 1;
-            if (element[i].suit !== element[j].suit){
-                sameSuit = false;
-                break
-            } else {
-                sameSuit = true;
-            }
-        }
-        // check what's true and false
-        if (consecutive === true && sameSuit === true){
-            if (element[4].value === 14) {
-                console.log('Royal Flush!');
-            } else {
-                console.log('Straight Flush!')
-            }
-        } else if (consecutive === true && sameSuit === false){
-            console.log('Straight!')
-        } else if (consecutive === false && sameSuit === true) {
-            console.log('Flush!')
-        } else {
-            console.log('keep checking');
-        }
-        //console.log(consecutive, sameSuit);
+        checkConsecutive(hand);
+        checkSameSuit(hand);
+        // add hands check here switch statement
     });
 }
+
 
 function getWinner(a,b){
     //compare players hands and determine winner
@@ -113,6 +155,7 @@ function getWinner(a,b){
 
 function init (hands){
     for (i=0; i<hands.length; i++){ //change to foreach
+        playerHands = [];
         var cards = getCardsObject(hands[i]);
         separatePlayers(cards);
         checkHands(playerCards);
@@ -122,9 +165,8 @@ function init (hands){
     process.exit(); // add option to run again or exit?
 }
 
-process.stdin.on('data', function(data){
+rl.input.on('data', function(data){
     var allData = data.toString().trim();
     allGames = allData.split('\r');
-    process.stdout.write('\n\n'); //just adding some padding
     init(allGames);  
 });
